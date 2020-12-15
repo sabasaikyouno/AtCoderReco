0e5503f6555d04137243e8ded139f9967cc45adf
sabasaikyouno <0852kuro@gmail.com>
2020-12-15 18:17:19 +0900

    Create a - twiblr.scala

---
+++ abc-182/a - twiblr.scala
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+object Main extends App {
+  val sc = new Scanner(System.in)
+  var a = sc.nextInt
+  var b = sc.nextInt
+  a = 2 * a + 100
+  var n = a - b
+  println(n)
+
+}
